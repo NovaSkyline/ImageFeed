5de5a19ea6558d2170f7c34fe7c3d5d0246163b1
Skyline <r_kamalov@mail.ru>
2024-11-04 19:49:17 +0200

    Initial Commit

---
+++ ImageFeedTests/ImageFeedTests.swift
@@ -0,0 +1,17 @@
+//
+//  ImageFeedTests.swift
+//  ImageFeedTests
+//
+//  Created by Руслан Камалов on 04.11.2024.
+//
+
+import Testing
+@testable import ImageFeed
+
+struct ImageFeedTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
