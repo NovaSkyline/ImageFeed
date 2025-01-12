0c265fc463b28a2168913333e43db3d42fbe0b90
Nova_Skyline <r_kamalov@mail.ru>
2025-01-12 02:52:11 +0200

    Добавлены и настроены 3 элемента: UIImageView — для основной картинки, UILabel — для даты, UIButton — для кнопки лайка.

---
+++ ImageFeed/ImageList/ImagesListCell.swift
@@ -0,0 +1,11 @@
+//
+//  ImagesListCell.swift
+//  Image Feed
+//
+//  Created by Руслан Камалов on 12.01.2025.
+//
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    static let reuseIdentifier = "ImagesListCell"
+}
