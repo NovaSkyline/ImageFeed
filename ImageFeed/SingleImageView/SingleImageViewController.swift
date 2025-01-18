b0b4456be1b3782a50edd4c270172d60e00d4def
Nova_Skyline <r_kamalov@mail.ru>
2025-01-18 18:36:03 +0200

    Добавлен и настроен по макету контроллер для показа одной картинки (SingleImageViewController).

---
+++ ImageFeed/SingleImageView/SingleImageViewController.swift
@@ -0,0 +1,12 @@
+//
+//  SingleImageViewController.swift
+//  Image Feed
+//
+//  Created by Руслан Камалов on 18.01.2025.
+//
+
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+}
